2d44d32271a9dc9c63692def73d42317e5b717dc
mdeandwibekti <mdeandwibekti@gmail.com>
2025-06-10 10:51:19 +0700

    menambahkan else pada void remove untuk jika elemen dihapus berada diakhir kembali ke awal array

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -45,6 +45,13 @@ class CircularQueue {
         if (FRONT == REAR) {
             FRONT = -1;
             REAR = -1;
+        }
+        else {
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
         
     }
 
