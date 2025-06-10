0baa705ee8ddda585fcad0e7fb60320cb20503a2
mdeandwibekti <mdeandwibekti@gmail.com>
2025-06-10 11:09:19 +0700

    menambahkan else pada void display untuk iterasi FRONT hingga akhir array

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -71,8 +71,12 @@ class CircularQueue {
             }
             cout << endl;
         }
-        
+        else {
+            while (FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            FRONT_position = 0;
+        }  
     }
-
-
-}
+};
