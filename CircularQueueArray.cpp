be92cc76a55de0063f6b28ab81ddbf2506f3197e
mdeandwibekti <mdeandwibekti@gmail.com>
2025-06-10 11:00:58 +0700

    membuat if untuk FRONT_position dan REAR_position iterasi dari front hingga REAR

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -62,6 +62,16 @@ class CircularQueue {
             cout << "Queue is empty\n";
             return;
         }
+        cout << "\nElements in the queue are ...\n";
+
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+        
     }
 
 
